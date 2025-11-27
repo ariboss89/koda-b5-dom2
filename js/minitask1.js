7bb79f2b2fdef5ebfc157228e245513addbb9e50
ariboss89 <ariboss89@gmail.com>
2025-11-27 23:14:26 +0700

    refactor: fixing logic function in minitask1.js

--- js/minitask1.js
+++ js/minitask1.js
@@ -1,12 +1,37 @@
-const pwd = document.querySelector("#pwd");
-console.log(pwd, "apaa");
-pwd.addEventListener("click", function () {
-  const test = pwd.getAttribute("type");
-  if (test == "password") {
-    pwd.setAttribute("type", "text");
-    pwd.setAttribute("class", "pasw eye-slash");
+const password = document.querySelector("input[name='password']");
+const eyeClicked = document.querySelector("#eyeClick");
+
+eyeClicked.addEventListener("click", function () {
+  const checkAtr = password.getAttribute("type");
+
+  if (checkAtr == "password") {
+    password.setAttribute("type", "text");
+    eyeClicked.setAttribute("src", "/icons/eye-slash.png");
   } else {
-    pwd.setAttribute("type", "password");
-    pwd.setAttribute("class", "pasw eye");
+    password.setAttribute("type", "password");
+    eyeClicked.setAttribute("src", "/icons/eye.png");
   }
 });
+
+function onClick() {
+  const hamburger = document.querySelector(".ham-btn");
+  const nav = document.querySelector(".nav-hero");
+
+  const getAtr = window.getComputedStyle(nav);
+  console.log(window.innerWidth, "aaaaaadda");
+
+  hamburger.addEventListener("click", function () {
+    getAtr.display == "none"
+      ? nav.setAttribute("style", "display:flex")
+      : nav.setAttribute("style", "display:none");
+
+    //console.log(getAtr.display);
+
+    // window.innerWidth < 768
+    //   ? nav.setAttribute("style", "display:none")
+    //   : nav.setAttribute("style", "display:flex");
+  });
+}
+
+onClick();
+//document.addEventListener("DOMContentLoaded", onclick);
