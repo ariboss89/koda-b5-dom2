e8c5ded6b172506e0d4dc3eb534af9baf62c86f4
ariboss89 <ariboss89@gmail.com>
2025-11-30 15:49:10 +0700

    feat: adding filtering function in pokemon.js

--- js/pokemon.js
+++ js/pokemon.js
@@ -19,16 +19,16 @@ const colours = {
   fairy: "#D685AD",
 };
 
-let allData = [];
-let arrUrl = [];
-
 let dataPokemon = {
   name: "name",
   image: "url",
   type: ["type"],
 };
 
-async function getPokemon() {
+let pokemonAll = [];
+let typePokemons = [];
+
+async function getPokemonWithAbilities() {
   const url = "https://pokeapi.co/api/v2/pokemon?limit=30&offset=0";
   try {
     const response = await fetch(url);
@@ -36,147 +36,161 @@ async function getPokemon() {
       throw new Error(`${response.status} ${response.statusText}`);
     }
     const data = await response.json();
-    const chars = data.results;
-    const target = document.querySelector("section.hero-main");
-    const fragment = document.createDocumentFragment();
-
-    chars.forEach((character) => {
-      const div = document.createElement("div");
-      div.classList.add("main-grid");
-      const name = document.createElement("div");
-      name.setAttribute("class", "item");
-      name.textContent = character.name.toUpperCase();
-      div.append(name);
-
-      const url = character.url;
-      // const pokeName = character.name;
-
-      arrUrl.push(url);
-
-      (async () => {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`${response.status} ${response.statusText}`);
+    const result = data.results.map(async (pokemon) => {
+      try {
+        const res = await fetch(pokemon.url);
+        if (!res.ok) {
+          throw new Error(`${res.status} ${res.statusText}`);
         }
-        const data = await response.json();
-        const types = data.types;
-
-        const divImg = document.createElement("div");
-        divImg.setAttribute("class", "item");
-        div.append(divImg);
-
-        const images = document.createElement("img");
-        images.setAttribute("class", "item");
-        images.setAttribute("src", data.sprites.front_default);
-        images.setAttribute("alt", `gambar ${character.name}`);
-        divImg.append(images);
-
-        const divBlack = document.createElement("div");
-        divBlack.setAttribute("class", "item");
-        div.append(divBlack);
-
-        const divType = document.createElement("div");
-        divType.setAttribute("class", "item");
-        div.append(divType);
-
-        dataPokemon.name = data.name;
-        dataPokemon.image = data.sprites.front_default;
-
-        for (let i = 0; i < types.length; i++) {
-          let variant = types[i].type.name;
-          for (const key in colours) {
-            if (variant == key) {
-              const divTypeData = document.createElement("div");
-              divTypeData.classList.add("flex-type");
-              divTypeData.style.backgroundColor = colours[key];
-              divTypeData.textContent = variant;
-              divType.append(divTypeData);
-
-              dataPokemon.type.push(variant);
-            }
-          }
-        }
-
-        allData.push(dataPokemon);
-        //
-      })();
-      fragment.append(div);
+        let groupPokemon = [];
+        const data2 = await res.json();
+        groupPokemon.push(data2);
+        return groupPokemon;
+      } catch (error) {
+        throw error;
+      }
     });
+    const pokemons = await Promise.all(result);
 
-    target.append(fragment);
-  } catch (error) {}
+    for (let i = 0; i < pokemons.length; i++) {
+      const pokedex = {
+        name: "",
+        image: "",
+        types: [],
+      };
+
+      pokedex.name = pokemons[i][0].name;
+      pokedex.image = pokemons[i][0].sprites.front_default;
+
+      for (let j = 0; j < pokemons[i][0].types.length; j++) {
+        pokedex.types.push(pokemons[i][0].types[j].type.name);
+
+        if (!typePokemons.includes(pokemons[i][0].types[j].type.name)) {
+          typePokemons.push(pokemons[i][0].types[j].type.name);
+        }
+      }
+      pokemonAll.push(pokedex);
+    }
+    createDiv(pokemonAll, typePokemons);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getPokemonWithAbilities();
+
+const target = document.querySelector("section.hero-main");
+let fragment = document.createDocumentFragment();
+
+function createDiv(dataPokemon, typePokemon) {
+  // tahap 2 membuat select
+  const divSelect = document.createElement("div");
+  divSelect.classList.add("flex-search");
+  const select = document.createElement("select");
+  select.setAttribute("class", "select");
+  select.setAttribute("id", "selectType");
+  divSelect.append(select);
+  const theOpt = document.createElement("option");
+  theOpt.value = "Choose Type Pokemon";
+  theOpt.textContent = "Choose Type Pokemon";
+  theOpt.selected = true;
+  theOpt.disabled = true;
+  select.appendChild(theOpt);
+
+  const allOpt = document.createElement("option");
+  allOpt.value = "All";
+  allOpt.textContent = "All";
+  select.appendChild(allOpt);
+
+  for (let i = 0; i < typePokemon.length; i++) {
+    const opt = document.createElement("option");
+    opt.value = typePokemon[i];
+    opt.textContent = typePokemon[i];
+    select.appendChild(opt);
+  }
+
+  fragment.append(divSelect);
+  target.append(fragment);
+
+  const selectPokemon = document.querySelector("#selectType");
+
+  if (selectPokemon.value === "Choose Type Pokemon") {
+    filterData(dataPokemon);
+  }
+
+  selectPokemon.addEventListener("change", () => {
+    if (
+      selectPokemon.value === "All" ||
+      selectPokemon.value === "Choose Type Pokemon"
+    ) {
+      filterData(dataPokemon);
+    } else {
+      let filterRes = [];
+      for (let l = 0; l < dataPokemon.length; l++) {
+        const isTrue = dataPokemon[l].types.includes(selectPokemon.value);
+
+        if (isTrue) {
+          filterRes.push(dataPokemon[l]);
+        }
+      }
+      filterData(filterRes);
+    }
+  });
 }
 
-getPokemon();
+function filterData(dataPokemon) {
+  const divToRemove = target.querySelector("#divMain");
 
-console.log(arrUrl, "bin");
+  if (divToRemove) {
+    divToRemove.remove();
+  }
 
-const urls = [
-  "https://pokeapi.co/api/v2/pokemon/1/",
-  "https://pokeapi.co/api/v2/pokemon/2/",
-  "https://pokeapi.co/api/v2/pokemon/3/",
-];
+  const divMain = document.createElement("div");
+  divMain.setAttribute("class", "hero-main");
+  divMain.setAttribute("id", "divMain");
 
-console.log(urls, "aa");
+  for (let j = 0; j < dataPokemon.length; j++) {
+    const div = document.createElement("div");
+    div.classList.add("main-grid");
+    const name = document.createElement("div");
+    name.setAttribute("class", "item");
+    name.textContent = dataPokemon[j].name.toUpperCase();
+    div.append(name);
 
-Promise.all(arrUrl.map((url) => fetch(url).then((response) => response.json())))
-  .then((data) => {
-    console.log("All data fetched successfully:", data);
-    // data will be an array like:
-    // [
-    //   { userId: 1, id: 1, title: 'delectus aut autem', completed: false },
-    //   { userId: 1, id: 2, title: 'quis ut nam facilis et officia qui', completed: false },
-    //   { userId: 1, id: 3, title: 'fugiat veniam minus', completed: false }
-    // ]
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
+    const divImg = document.createElement("div");
+    divImg.setAttribute("class", "item");
+    div.append(divImg);
 
-// const fetchPromises = arrUrl.map((url) => fetch(url));
+    const images = document.createElement("img");
+    images.setAttribute("class", "item");
+    images.setAttribute("src", dataPokemon[j].image);
+    images.setAttribute("alt", `gambar ${dataPokemon[j].name}`);
+    divImg.append(images);
 
-// Promise.all(fetchPromises).then((values) => {
-//   console.log(values, "aa");
-// });
+    const divBlack = document.createElement("div");
+    divBlack.setAttribute("class", "item");
+    div.append(divBlack);
 
-// Promise.all(arrUrl.map((url) => fetch(url).then((response) => response.json())))
-//   .then((data) => {
-//     console.log("All data fetched successfully:", data);
-//   })
-//   .catch((error) => {
-//     console.error("Error fetching data:", error);
-//   });
+    const divType = document.createElement("div");
+    divType.setAttribute("class", "item");
+    div.append(divType);
 
-// Promise.all(fetchPromises)
-//   .then((responses) => {
-//     // All fetches completed successfully
-//     // 'responses' is an array of Response objects
-//     // You can further process each response, e.g., by calling .json()
-//     return Promise.all(responses.map((response) => response.json()));
-//   })
-//   .then((data) => {
-//     // 'data' is an array of JSON objects from all successful responses
-//     console.log("All data fetched:", data);
-//   })
-//   .catch((error) => {
-//     // Handle any errors that occurred during fetching or parsing
-//     console.error("Error fetching data:", error);
-//   });
-
-//const pwd = document.querySelector("#selectType");
-
-//pwd.addEventListener("change", function () {
-// const test = pwd.getAttribute("type");
-// if (test == "password") {
-//   pwd.setAttribute("type", "text");
-//   pwd.setAttribute("class", "pasw eye-slash");
-// } else {
-//   pwd.setAttribute("type", "password");
-//   pwd.setAttribute("class", "pasw eye");
-// }
-
-//console.log(pwd.value, "apaa");
-//});
+    for (let k = 0; k < dataPokemon[j].types.length; k++) {
+      let variant = dataPokemon[j].types[k];
+      for (const key in colours) {
+        if (variant == key) {
+          const divTypeData = document.createElement("div");
+          divTypeData.classList.add("flex-type");
+          divTypeData.style.backgroundColor = colours[key];
+          divTypeData.textContent = variant;
+          divType.append(divTypeData);
+        }
+      }
+    }
+    divMain.append(div);
+    fragment.append(divMain);
+    target.append(fragment);
+  }
+}
 
 const btnLogout = document.querySelector("#navLogout");
 btnLogout.addEventListener("click", () => {
@@ -185,4 +199,77 @@ btnLogout.addEventListener("click", () => {
   location.replace("/views/login.html");
 });
 
-console.log(btnLogout, "aaa");
+// async function getPokemon() {
+//   const url = "https://pokeapi.co/api/v2/pokemon?limit=30&offset=0";
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`${response.status} ${response.statusText}`);
+//     }
+//     const data = await response.json();
+//     const chars = data.results;
+//     const target = document.querySelector("section.hero-main");
+//     const fragment = document.createDocumentFragment();
+
+//     chars.forEach((character) => {
+//       const div = document.createElement("div");
+//       div.classList.add("main-grid");
+//       const name = document.createElement("div");
+//       name.setAttribute("class", "item");
+//       name.textContent = character.name.toUpperCase();
+//       div.append(name);
+
+//       const url = character.url;
+
+//       (async () => {
+//         const response = await fetch(url);
+//         if (!response.ok) {
+//           throw new Error(`${response.status} ${response.statusText}`);
+//         }
+//         const data = await response.json();
+//         const types = data.types;
+
+//         const divImg = document.createElement("div");
+//         divImg.setAttribute("class", "item");
+//         div.append(divImg);
+
+//         const images = document.createElement("img");
+//         images.setAttribute("class", "item");
+//         images.setAttribute("src", data.sprites.front_default);
+//         images.setAttribute("alt", `gambar ${character.name}`);
+//         divImg.append(images);
+
+//         const divBlack = document.createElement("div");
+//         divBlack.setAttribute("class", "item");
+//         div.append(divBlack);
+
+//         const divType = document.createElement("div");
+//         divType.setAttribute("class", "item");
+//         div.append(divType);
+
+//         dataPokemon.name = data.name;
+//         dataPokemon.image = data.sprites.front_default;
+
+//         for (let i = 0; i < types.length; i++) {
+//           let variant = types[i].type.name;
+//           for (const key in colours) {
+//             if (variant == key) {
+//               const divTypeData = document.createElement("div");
+//               divTypeData.classList.add("flex-type");
+//               divTypeData.style.backgroundColor = colours[key];
+//               divTypeData.textContent = variant;
+//               divType.append(divTypeData);
+
+//               dataPokemon.type.push(variant);
+//             }
+//           }
+//         }
+//       })();
+
+//       fragment.append(div);
+//     });
+
+//     target.append(fragment);
+//   } catch (error) {}
+// }
+// //getPokemon();
